acc750485d6cbf7d90a393cf1a271cc2a6272d59
Vaibhav Arora <aroravaibhav817@gmail.com>
2022-10-13 21:35:51 +0530

    deployed to goerli

--- solidity/hardhat.config.js
+++ solidity/hardhat.config.js
@@ -3,15 +3,15 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const INFURA_URL_API = process.env.ROPSTEN_URL_API;
+const INFURA_URL_API = process.env.GOERLI_URL_API;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    ropsten: {
+    goerli: {
       url: INFURA_URL_API,
-      accounts: [PRIVATE_KEY], 
+      accounts: [PRIVATE_KEY] 
     }
   }
 };
